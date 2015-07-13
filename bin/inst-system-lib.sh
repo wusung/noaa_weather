8e7c5d5ee1379337820112d49359521b22f57c7e
Wusung Peng <wusung.peng@kyerpdata.com>
2015-07-13 17:03:22 +0800

    Use vagrant

---
+++ bin/inst-system-lib.sh
@@ -0,0 +1,9 @@
+apt-get update
+apt-get install -y linux-image-generic-lts-trusty
+wget -qO- https://get.docker.com/gpg | sudo apt-key add -
+wget -qO- https://get.docker.com/ | sh
+
+apt-get install -y default-jre
+apt-get install -y default-jdk
+
+apt-get install -y maven
